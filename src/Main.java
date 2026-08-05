319c4c09aad5ac6b255aa1ae68f14c5dfe4bd0bc
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-05 08:52:50 -0300

    Instanciando o objeto e preenchendo os atribuitos

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Filme filme1 = new Filme();
+
+        filme1.nome="Duna";
+        filme1.anoDeLancamento=2023;
+        filme1.duracaoEmMinuitos=166;
+        filme1.incluidoNoPlano=true;
+
+        filme1.exibeFichaTecnica();
 
     }
 }
