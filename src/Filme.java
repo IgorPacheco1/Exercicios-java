5530e166e61ec7d6fbfb7c330c95888a380ef547
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-05 08:32:50 -0300

    Definindo atributos da classe Filme

---
+++ src/Filme.java
@@ -0,0 +1,11 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    int duracaoEmMinuitos;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+
+
+
+}
