a7ada2fc2a9c42f9bd1d25003bd4c73cdc2f1885
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-05 09:39:30 -0300

    Instanciando todas as classes e fazendo testes

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,30 @@ public class Main {
     public static void main(String[] args) {
 
         Filme filme1 = new Filme();
+        Pessoa pessoa1 = new Pessoa();
+        Calculadora calculadora1 = new Calculadora();
+        Musica musica1 = new Musica();
+        Aluno aluno1 = new Aluno();
 
-        filme1.nome="Duna";
-        filme1.anoDeLancamento=2023;
-        filme1.duracaoEmMinuitos=166;
-        filme1.incluidoNoPlano=true;
+        pessoa1.saudacao();
+        System.out.println("-----------------------------");
+        double resultado = calculadora1.multiplica(25);
+        System.out.println(resultado);
+        System.out.println("-----------------------------");
+        musica1.titulo = "zazao promoter";
+        musica1.artista = "zazao";
+        musica1.anoDeLancamento = 2026;
+        musica1.avalia(5);
+        musica1.avalia(4.4);
+        musica1.avalia(4.3);
+        musica1.exibirFicha();
+        System.out.println("-----------------------------");
+        aluno1.nome = "IGOR";
+        aluno1.idade = 22;
+        aluno1.exibirDados();
 
-        filme1.avalia(4.9);
-        filme1.avalia(4.8);
-        filme1.avalia(5);
-        filme1.exibeFichaTecnica();
+
+
 
 
     }
