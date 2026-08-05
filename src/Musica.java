cee4861fc49baf5784d4f82ca6b7b6ed8b09af35
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-05 09:16:31 -0300

    Criação da classe musica, definindo seus atribuitos e metodos

---
+++ src/Musica.java
@@ -0,0 +1,29 @@
+public class Musica {
+
+    String titulo;
+    String artista;
+    int anoDeLancamento;
+    double avaliacao;
+    int numAvaliacoes;
+
+
+    void exibirFicha(){
+        System.out.println("==========================");
+        System.out.println("FICHA TECNICA");
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Artista: " + artista);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Avaliação: " + avaliacaoMedia());
+        System.out.println("Quantidades de avaliações: " + numAvaliacoes);
+    }
+
+    void avalia(double nota){
+        avaliacao += nota;
+        numAvaliacoes++;
+    }
+
+    double avaliacaoMedia(){
+        double media = avaliacao / numAvaliacoes;
+        return media;
+    }
+}
