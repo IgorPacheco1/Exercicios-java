410cd068d6c41d8fa5826ec5c6d3f56d62ef7deb
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-06 19:40:34 -0300

    Criando os getters e setters

--- src/Filme.java
+++ src/Filme.java
@@ -1,14 +1,55 @@
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    int duracaoEmMinuitos;
-    boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
+    private String nome;
+    private int anoDeLancamento;
+    private int duracaoEmMinuitos;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public int getDuracaoEmMinuitos() {
+        return duracaoEmMinuitos;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinuitos(int duracaoEmMinuitos) {
+        this.duracaoEmMinuitos = duracaoEmMinuitos;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+        this.totalDeAvaliacoes = totalDeAvaliacoes;
+    }
+
+    private int totalDeAvaliacoes;
 
 
 
-    void exibeFichaTecnica(){
+    public void exibeFichaTecnica(){
         System.out.println("==================================");
         System.out.println("FICHA TÉCNICA");
         System.out.println("Nome: " + nome);
@@ -19,12 +60,12 @@ public class Filme {
         System.out.println("==================================");
     }
 
-    void avalia(double nota){
+   public void avalia(double nota){
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
     }
 
-    double pegaMedia(){
+   public double pegaMedia(){
         double media = somaDasAvaliacoes / totalDeAvaliacoes;
         return media;
     }
