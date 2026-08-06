ccd82d524cb6e91ccfc26dfd045383268f43b692
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-06 20:28:29 -0300

    Criação de: classe Produto / atributos / getters e setters / metodo de aplicação de desconto ao preço

---
+++ src/Produto.java
@@ -0,0 +1,29 @@
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double setPreco(double preco) {
+        this.preco = preco;
+        return preco;
+    }
+
+    public double aplicarDesconto(double desconto){
+        double valorDesconto = (desconto / 100) * getPreco();
+        double valorFinal = getPreco() - valorDesconto;
+
+        return setPreco(valorFinal);
+    }
+}
