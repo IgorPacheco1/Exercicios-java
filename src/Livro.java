91cf427573d03ad3f2f3e38f4bcedb5d86cd4359
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-06 21:20:25 -0300

    Criação da classe Livro / atributos / getters setters / metodo exibirDetalhes

---
+++ src/Livro.java
@@ -0,0 +1,17 @@
+public class Livro {
+
+    private String autor;
+    private String titulo;
+
+    public Livro(String autor,String titulo){
+        this.autor = autor;
+        this.titulo = titulo;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("DADOS DO LIVRO");
+        System.out.println("");
+        System.out.println("Autor " + autor);
+        System.out.println("Titulo " + titulo);
+    }
+}
