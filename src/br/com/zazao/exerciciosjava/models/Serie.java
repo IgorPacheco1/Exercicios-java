3ad5f34c9113984f0ebdea9ee5149a750722424a
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-08 20:08:39 -0300

    Adicionando o atributo / getters e setters / Override metodo getDuracaoEmMinutos

--- src/br/com/zazao/exerciciosjava/models/Serie.java
+++ src/br/com/zazao/exerciciosjava/models/Serie.java
@@ -3,6 +3,7 @@ package br.com.zazao.exerciciosjava.models;
 public class Serie extends Titulo{
     private int numeroDeEpsodio;
     private int numeroDeTemporadas;
+    private int minutosPorEpisodio;
 
     public int getNumeroDeEpsodio() {
         return numeroDeEpsodio;
@@ -18,5 +19,18 @@ public class Serie extends Titulo{
 
     public void setNumeroDeTemporadas(int numeroDeTemporadas) {
         this.numeroDeTemporadas = numeroDeTemporadas;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return numeroDeTemporadas * numeroDeEpsodio * minutosPorEpisodio;
     }
 }
