298660c340106d2de04d74e330a576b44731a94b
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-08 20:06:55 -0300

    realizando testes para verificar o funcionamento

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,40 @@
-import br.com.zazao.exerciciosjava.models.*;
+import br.com.zazao.exerciciosjava.calculos.CalculadoraDeTempo;
+import br.com.zazao.exerciciosjava.models.Serie;
+import br.com.zazao.exerciciosjava.models.Filme;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+
         Filme filme1 = new Filme();
-        Pessoa pessoa1 = new Pessoa();
-        Calculadora calculadora1 = new Calculadora();
-        Musica musica1 = new Musica();
-        Aluno aluno1 = new Aluno("igor",15,25,10);
-        IdadePessoa idade = new IdadePessoa();
-        Produto produto1 = new Produto();
-        Livro livro =  new Livro("CR7","O prime supremo");
+        filme1.setNome("Minions");
+        filme1.setAnoDeLancamento(2016);
+        filme1.setDuracaoEmMinutos(120);
+        filme1.setIncluidoNoPlano(true);
 
-        livro.exibirDetalhes();
+        Filme filme2 = new Filme();
+        filme2.setNome("mafia holandesa");
+        filme2.setAnoDeLancamento(2016);
+        filme2.setDuracaoEmMinutos(120);
+        filme2.setIncluidoNoPlano(true);
 
+        Serie serie1 = new Serie();
+        serie1.setNumeroDeEpsodio(180);
+        serie1.setMinutosPorEpisodio(24);
+        serie1.setNome("hunterxhunter");
+        serie1.setIncluidoNoPlano(true);
+        serie1.setNumeroDeTemporadas(3);
 
+        CalculadoraDeTempo calcTempo = new CalculadoraDeTempo();
 
+        calcTempo.inclui(filme1);
+        calcTempo.inclui(filme2);
+        calcTempo.inclui(serie1);
 
-
-
-
-
+        System.out.println("Tempo total para assisitir " + calcTempo.getTempoTotal());
 
 
     }
