540814f7755f498de7720d21449876f8ad931b08
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-09 15:26:52 -0300

    Criando metodos para usar nas subclasses e ter melhor acesso, alem de excluir set desnecessario

--- src/br/com/zazao/exerciciosjava/models/ContaBancaria.java
+++ src/br/com/zazao/exerciciosjava/models/ContaBancaria.java
@@ -17,8 +17,20 @@ public class ContaBancaria {
         this.numeroConta = numeroConta;
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
+    public void depositar(double valor){
+        saldo += valor;
     }
+
+    public void sacar(double valor){
+        if (valor>saldo){
+            System.out.println("Saldo insuficiente para realizar operação");
+        }else saldo -= valor;
+    }
+
+    public void consultaSaldo(){
+        System.out.println(saldo);
+    }
+
+
 
 }
