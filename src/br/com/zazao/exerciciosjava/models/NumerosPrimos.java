fccb49f9802b0292c5b9f4c4a498ccf9f1bae65e
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-09 16:24:57 -0300

    Classe criada porem função não realizada com sucesso

---
+++ src/br/com/zazao/exerciciosjava/models/NumerosPrimos.java
@@ -0,0 +1,37 @@
+package br.com.zazao.exerciciosjava.models;
+
+public class NumerosPrimos {
+
+    public void verificarPrimalidade(int numero){
+        if (numero % 2 == 0 && numero > 2 ){
+            System.out.println("Esse numero não é primo");
+        }else System.out.println("Esse numero é primo");
+    }
+
+    public void listarPrimos(){
+
+
+
+    }
+
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFAFALHEI EM RESOLVER ESSA TAREFAVFALHEI EM RESOLVER ESSA TAREFAFALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+//    FALHEI EM RESOLVER ESSA TAREFA
+
+
+
+
+}
