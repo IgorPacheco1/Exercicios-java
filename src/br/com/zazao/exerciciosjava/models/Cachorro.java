cec8484f94223392cda3594e1bc27e14e28bf091
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-09 15:06:12 -0300

    Criando a classe e fazendo override do metodo da superclasse

---
+++ src/br/com/zazao/exerciciosjava/models/Cachorro.java
@@ -0,0 +1,11 @@
+package br.com.zazao.exerciciosjava.models;
+
+public class Cachorro extends Animal{
+    @Override
+    public void emitirSom() {
+        System.out.println("AU AU");
+    }
+    private void abanarRabo(){
+
+    }
+}
