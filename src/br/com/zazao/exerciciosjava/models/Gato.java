096961386729acd842956a1dc48bec56979f369d
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-09 15:06:08 -0300

    Criando a classe e fazendo override do metodo da superclasse

---
+++ src/br/com/zazao/exerciciosjava/models/Gato.java
@@ -0,0 +1,12 @@
+package br.com.zazao.exerciciosjava.models;
+
+public class Gato extends Animal{
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Miauuuuu");
+    }
+    private void arranharMoveis(){
+
+    }
+}
