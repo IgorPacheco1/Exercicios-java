af9338f072f115b459434ab9e305fd49eb045d85
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-09 15:05:30 -0300

    Realizando testes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.zazao.exerciciosjava.models.Cachorro;
 import br.com.zazao.exerciciosjava.models.Carro;
+import br.com.zazao.exerciciosjava.models.Gato;
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,23 +8,10 @@ import br.com.zazao.exerciciosjava.models.Carro;
 public class Main {
     public static void main(String[] args) {
 
-
-        Carro carro1 = new Carro();
-        carro1.setAnoModelo(2012);
-        carro1.setNomeDoModelo("Corolla XLi 1.8 (Flex)");
-        carro1.setPrecoMedioPorAno(58100);
-
-        Carro carro2 = new Carro();
-        carro2.setAnoModelo(2013);
-        carro2.setNomeDoModelo("Corolla XLi 1.8 (Flex)");
-        carro2.setPrecoMedioPorAno(63884);
-
-        Carro carro3 = new Carro();
-        carro3.setAnoModelo(2014);
-        carro3.setNomeDoModelo("Corolla XLi 1.8 (Flex)");
-        carro3.setPrecoMedioPorAno(65482);
-
-        carro1.exibirDados();
+        Cachorro dog =  new Cachorro();
+        Gato gat = new Gato();
+        dog.emitirSom();
+        gat.emitirSom();
 
     }
 }
