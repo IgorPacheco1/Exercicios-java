66ccd15d701e82ec2d18b169786862525d98a0b5
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-12 14:05:01 -0300

    Criando a interface para definir o molde para a classe principal e definindo parametros

---
+++ src/br/com/zazao/exerciciosjava/calculos/CalculoGeometrico.java
@@ -0,0 +1,11 @@
+package br.com.zazao.exerciciosjava.calculos;
+
+public interface CalculoGeometrico {
+
+    default void calcularArea(double altura, double largura){
+
+    }
+    default void calcularPerimetro(double altura, double largura){
+
+    }
+}
