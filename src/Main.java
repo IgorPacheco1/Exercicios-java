6aa23da927125e28eecc430150b24f000581a518
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-12 13:51:26 -0300

    Realizando testes

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-import br.com.zazao.exerciciosjava.models.Cachorro;
-import br.com.zazao.exerciciosjava.models.Carro;
-import br.com.zazao.exerciciosjava.models.Gato;
+import br.com.zazao.exerciciosjava.models.*;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Cachorro dog =  new Cachorro();
-        Gato gat = new Gato();
-        dog.emitirSom();
-        gat.emitirSom();
+    ConversorMoeda c = new ConversorMoeda();
+    c.converterDolarParaReal(50);
 
     }
 }
