e3db8d728f362f59378af1280fac33cb723ca533
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-12 14:44:54 -0300

    Criando atributo valor / getters setters / override para o implements Calculavel

--- src/br/com/zazao/exerciciosjava/models/Livro.java
+++ src/br/com/zazao/exerciciosjava/models/Livro.java
@@ -1,13 +1,42 @@
 package br.com.zazao.exerciciosjava.models;
 
-public class Livro {
+import br.com.zazao.exerciciosjava.calculos.Calculavel;
+
+public class Livro implements Calculavel {
 
     private String autor;
     private String titulo;
+    private double valor;
 
-    public Livro(String autor,String titulo){
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+    public Livro(String autor,String titulo,double valor){
         this.autor = autor;
         this.titulo = titulo;
+        this.valor = valor;
     }
 
     public void exibirDetalhes(){
@@ -15,5 +44,12 @@ public class Livro {
         System.out.println("");
         System.out.println("Autor " + autor);
         System.out.println("Titulo " + titulo);
+    }
+
+    @Override
+    public void calcularPrecoFinal(double valorDesconto) {
+        double precoFinal = getValor() - valorDesconto;
+        System.out.println("Preço antingo: " + getValor() + " Preço com desconto " + precoFinal);
+
     }
 }
