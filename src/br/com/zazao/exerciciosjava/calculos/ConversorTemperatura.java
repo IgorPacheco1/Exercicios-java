3878756e3bb6cd94b14eb0ede474e1d1cf890e29
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-12 14:27:43 -0300

    Criando a interface

---
+++ src/br/com/zazao/exerciciosjava/calculos/ConversorTemperatura.java
@@ -0,0 +1,11 @@
+package br.com.zazao.exerciciosjava.calculos;
+
+public interface ConversorTemperatura {
+        void celsiusParaFahrenheit(double valor);
+        void fahrenheitParaCelsius(double valor);
+
+    }
+
+
+
+
