5023c63e1fa458bfb8a8e1ca270a01194cdae96b
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-12 14:04:20 -0300

    Realizando testes

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import br.com.zazao.exerciciosjava.models.*;
 public class Main {
     public static void main(String[] args) {
 
-    ConversorMoeda c = new ConversorMoeda();
-    c.converterDolarParaReal(50);
+        ConversorMoeda c = new ConversorMoeda();
+        c.converterDolarParaReal(50);
+        CalculadoraSalaRetangular sala = new CalculadoraSalaRetangular();
+        sala.calcularArea(5,3);
+        sala.calcularPerimetro(7,3);
 
     }
 }
