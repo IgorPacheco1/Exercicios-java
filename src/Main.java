31e0ec4a32c3735d71b7a8f7349730cece8434a0
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-14 13:54:52 -0300

    Realizando testes

--- src/Main.java
+++ src/Main.java
@@ -20,27 +20,45 @@ public class Main {
 //        livro.calcularPrecoFinal(20);
 
 
-        Pessoa manuel = new Pessoa();
-        manuel.setNome("Manuel");
-        manuel.setIdade(57);
+//        Pessoa manuel = new Pessoa();
+//        manuel.setNome("Manuel");
+//        manuel.setIdade(57);
+//
+//        Pessoa yasmin = new Pessoa();
+//        yasmin.setNome("Yasmin");
+//        yasmin.setIdade(21);
+//
+//        Pessoa paulo = new Pessoa();
+//        paulo.setNome("Paulo");
+//        paulo.setIdade(20);
+//
+//        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+//
+//        listaDePessoas.add(manuel);
+//        listaDePessoas.add(yasmin);
+//        listaDePessoas.add(paulo);
+//        System.out.println(listaDePessoas.size());
+//        System.out.println(listaDePessoas.get(1));
+//        System.out.println("");
+//        listaDePessoas.forEach(pessoa -> System.out.println("Nome:" + pessoa.getNome() + " Idade: " + pessoa.getIdade()));
 
-        Pessoa yasmin = new Pessoa();
-        yasmin.setNome("Yasmin");
-        yasmin.setIdade(21);
 
-        Pessoa paulo = new Pessoa();
-        paulo.setNome("Paulo");
-        paulo.setIdade(20);
+        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
 
-        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+        Produto produto1 = new Produto(5.99,"Biscoito", 10);
+        Produto produto2 = new Produto(8.99,"chocolate", 1);
+        Produto produto3 = new Produto(0.25,"bala", 30);
 
-        listaDePessoas.add(manuel);
-        listaDePessoas.add(yasmin);
-        listaDePessoas.add(paulo);
-        System.out.println(listaDePessoas.size());
-        System.out.println(listaDePessoas.get(1));
-        System.out.println("");
-        listaDePessoas.forEach(pessoa -> System.out.println("Nome:" + pessoa.getNome() + " Idade: " + pessoa.getIdade()));
+        listaDeProdutos.add(produto1);
+        listaDeProdutos.add(produto2);
+        listaDeProdutos.add(produto3);
+        System.out.println(listaDeProdutos.size());
+        System.out.println(listaDeProdutos.get(2));
+        System.out.println(listaDeProdutos);
+
+        ProdutoPerecivel produto4 = new ProdutoPerecivel(15.99,"Arroz",15,"01/01/2028");
+
+        System.out.println(produto4);
 
     }
 }
